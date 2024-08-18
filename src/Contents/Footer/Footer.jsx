7a0ffac7b878e5151d25228a262ca0f12ad8af7f
Jishnuuu-k <krishnakumarodappanal@gmail>
2024-08-18 21:41:04 +0530

    Banner Added

--- src/Contents/Footer/Footer.jsx
+++ src/Contents/Footer/Footer.jsx
@@ -3,43 +3,16 @@ import './footer.css'
 function Footer() {
   return (
     <div className='Footer'>
-      <div className="ContentBox1">
-        <div className="Contents01">
-          <h3>TEXTILES</h3>
-          <p>Location . . </p>
-          <p>Textiles@gmail.com</p>
-        </div>
-        <div className="Contents02">
-            <div className="FooterCard">
-              <h4>Texttiles</h4>
-              <div>Home</div>
-              <div>About Us</div>
-              <div>Products</div>
-              <div>Contact</div>
-            </div>
-            <div className="FooterCard">
-              <h4>Services</h4>
-              <div>content</div>
-              <div>content</div>
-              <div>content</div>
-              <div>content</div>
-            </div>
-            <div className="FooterCard">
-              <h4>Follow</h4>
-              <div class="SocialMedia">
-                <a href="https://www.instagram.com/ap.puuu"  class="SocialIcon">
-                <img src="./images/instagram.png" alt="Instagram"/>
-                </a>
-                <a href="https://www.facebook.com/jishnu.k.3572"  class="SocialIcon">
-                <img src="./images/facebook.png" alt="Facebook"/>
-                </a>
-              </div>
-            </div>
-        </div>
-      </div>
-      <div className="ContentBox2">
+        <div className="FooterSection1">
 
-      </div>
+        </div>
+        <div className="FooterSection2">
+            <div className="MessageBox">
+                <input type="text" placeholder='Enter Name' className='Data'/> <input type="email" placeholder='Enter Email' className='Data'/><br />
+                <input type="text" placeholder='Write Your Message Here' className='Feedback'/><br />
+                <button>Submit</button>
+            </div>
+        </div>
     </div>
   )
 }
