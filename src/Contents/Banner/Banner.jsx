e922b7711eb2f1826db2c10e5850376962f4392c
Akshaymm1001 <akshymm1001@gmail.com>
2024-08-22 11:59:54 +0530

    abbed banner

--- src/Contents/Banner/Banner.jsx
+++ src/Contents/Banner/Banner.jsx
@@ -5,13 +5,19 @@ function Banner() {
     <div className="Banner">
       <div className="SlideShow">
         <div className="BoxinsideSlideshow">
-
+         <div className="SubBox">
+        <div className="Note"><p><b>Welcome to Threads of Enchantment, where fabric meets fantasy. Discover a world of luxurious silks, vibrant velvets, and intricate laces, each crafted to spark your creativity. Our curated collection and expert staff turn every visit into an inspiring journey. Dive into the extraordinary and let your imagination run wild!</b></p></div>
+        <img  className='BannerImage' src="/picture/banner2.png" alt="img" />
         </div>
+        </div>
+       
       </div>
     <h2>NEWLY ARRIVED</h2>
       <div className="NewlyArrived">
         <div className="Cards">
-          <div className="CardImage"></div>
+          <div className="CardImage">
+            <img className='Img' src="https://i.pinimg.com/236x/68/bf/4f/68bf4f4956621a8904f8a816f11d4c2c.jpg" alt="" />
+          </div>
           <div className="CardDetails">
             <p>Name : Dress!</p>
             <p>Brand :</p>
@@ -43,7 +49,7 @@ function Banner() {
           </div>
         </div>
         <div className="Cards">
-        <div className="CardImage"></div>
+        <div className="CardImage"><img  className='Img' src="https://getethnic.com/wp-content/uploads/2023/12/30-Trending-Bridal-Lehengas-For-2024-Wedding_1.webp" alt="" /></div>
           <div className="CardDetails">
             <p>Name : Dress!</p>
             <p>Brand :</p>
